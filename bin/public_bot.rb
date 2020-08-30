2e21aa63e59c07daa9f89868b111eb721f23cf93
Luis Novoa <luis.matte.novoa@gmail.com>
2020-08-30 10:29:07 -0400

    Adds message responses to public_bot.rb

--- bin/public_bot.rb
+++ bin/public_bot.rb
@@ -43,7 +43,16 @@ Telegram::Bot::Client.run(token) do |bot|
         Telegram::Bot::Types::ReplyKeyboardMarkup
         .new(keyboard: @kb_home, one_time_keyboard: true)
       bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
-      user_participations = Participation.where(id: @identity)
+      user_participations = Participation.where(id: @identity).includes(:dashboard)
+      if user_participations.size.zero?
+        bot.api.send_message(chat_id: message.chat.id, text: "User #{@identity} isn't participating in any board")
+      else
+        text = ""
+        user_participations.each do |participation|
+          text += "Participation ID: #{participation.id}, Type: #{participation.dashboard.type}, Entrance Fee: #{participation.dashboard.entrance_fee}, Level: #{participation.level}\n"
+        end
+        bot.api.send_message(chat_id: message.chat.id, text: text)
+      end
     
     # Section 3 Volunteering
     when "#{@sec3}"
@@ -105,8 +114,13 @@ Telegram::Bot::Client.run(token) do |bot|
       # It should be able to create a new instance for that user based on those two parameters
       # aditionally, the user's unique id is found in @identity
       dashboard = Dashboard.find_by(type: @length, entrance_fee: @size)
-      dashboard.participations.build(user_id: @identity, level: 1, active: false)
-      dashboard.save
+      if dashboard.participations.where(user_id: @identity, active: true).zero?
+        dashboard.participations.build(user_id: @identity, level: 1, active: false)
+        dashboard.save
+        bot.api.send_message(chat_id: message.chat.id, text: "Your participation request was created. Please, pay the fee and notify the administrator.")
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: "You already participate on this dashboard! Please select another one.")
+      end
 
     when "#{@item_5_2}"
       question = @item_5_2_text
